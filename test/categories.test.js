0b81912531037e0382a4196565ebe851d9f35d39
WNoemie <noemiewolf@gmail.com>
2023-07-26 16:47:04 +0200

    categorie test get

--- test/categories.test.js
+++ test/categories.test.js
@@ -14,6 +14,22 @@ describe('Categories Routes', () => {
     server.close();
   });
 
+  describe('GET /api/categories', () => {
+    it('should return all categories from db', async () => {
+      await Categorie.create({ naam: 'Winter', beschrijving: 'Winter Collecite' });
+  
+      const res = await request(server).get('/api/categories');
+  
+    expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('array');
+      expect(res.body.length).to.equal(1);
+  
+      expect(res.body[0].naam).to.equal('Winter');
+      expect(res.body[0].beschrijving).to.equal('Winter Collecite');
+    });
+  });
+  
+
   
 
 });
