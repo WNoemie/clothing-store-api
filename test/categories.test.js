d8b2e6934e90880aeb410ed0dc5a307ac98aaf5b
WNoemie <noemiewolf@gmail.com>
2023-07-26 16:48:38 +0200

    insertmany

--- test/categories.test.js
+++ test/categories.test.js
@@ -16,19 +16,16 @@ describe('Categories Routes', () => {
 
   describe('GET /api/categories', () => {
     it('should return all categories from db', async () => {
-      await Categorie.create({ naam: 'Winter', beschrijving: 'Winter Collecite' });
-  
+      await Categorie.insertMany([
+        { naam: 'Winter', beschrijving: 'Winter Collecite' },
+        { naam: 'Zomer', beschrijving: 'Zomer Collectie' },
+      ]);
       const res = await request(server).get('/api/categories');
-  
-    expect(res.status).to.equal(200);
-      expect(res.body).to.be.an('array');
-      expect(res.body.length).to.equal(1);
-  
+      expect(res.status).to.equal(200);
       expect(res.body[0].naam).to.equal('Winter');
-      expect(res.body[0].beschrijving).to.equal('Winter Collecite');
+      expect(res.body[1].naam).to.equal('Zomer');
     });
   });
-  
 
   
 
