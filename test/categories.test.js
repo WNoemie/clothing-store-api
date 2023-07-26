460c68b45ecd342981e5d352330854895f966d75
WNoemie <noemiewolf@gmail.com>
2023-07-26 17:18:48 +0200

    post test

--- test/categories.test.js
+++ test/categories.test.js
@@ -2,6 +2,7 @@
 const request = require('supertest');
 const { expect } = require('chai');
 const { Categorie } = require('../models/categorie');
+const existingToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGMwNjVjZTc5YjNlNDU5NmU5MTdlMWYiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2OTAzNjcwNzZ9.JBTKNtbrdztoyxmIEoJ_rtwEB5qrhdBKyIOAfnkZquc';
 
 let server; 
 
@@ -46,6 +47,41 @@ describe('Categories Routes', () => {
         const response = await request(server).get(`/api/categories/${invalidId}`);
         expect(response.status).to.equal(404);
       });
+  });
+
+
+  describe('POST /api/categories', () => {
+    it('should create a new category if request is valid', async () => {
+      const categoryData = {
+        naam: 'Winter',
+        beschrijving: 'Winter Collectie',
+      };
+
+      const res = await request(server)
+        .post('/api/categories')
+        .set('Content-type', 'application/json')
+        .set('x-auth-token', existingToken) 
+        .send(categoryData);
+
+      expect(res.status).to.equal(200);
+      expect(res.body.naam).to.equal(categoryData.naam);
+      expect(res.body.beschrijving).to.equal(categoryData.beschrijving);
+
+      const category = await Categorie.findOne({ naam: categoryData.naam });
+      expect(category).to.exist;
+      expect(category.naam).to.equal(categoryData.naam);
+      expect(category.beschrijving).to.equal(categoryData.beschrijving);
+    });
+
+    it('should return 400 if request is invalid', async () => {
+      const res = await request(server)
+        .post('/api/categories')
+        .set('Content-type', 'application/json')
+        .set('x-auth-token', existingToken) 
+        .send({});
+
+      expect(res.status).to.equal(400);
+    });
   });
 
   
