fe103ce58bc07c84f82212f2d15cee744d0cc27a
WNoemie <noemiewolf@gmail.com>
2023-07-26 16:42:26 +0200

    before en after voor test

--- test/categories.test.js
+++ test/categories.test.js
@@ -1,1 +1,19 @@
 
+const request = require('supertest');
+const { expect } = require('chai');
+const { Categorie } = require('../models/categorie');
+
+let server; 
+
+describe('Categories Routes', () => {
+  beforeEach(() => {server = require('../index');;
+  });
+
+  afterEach(async () => {
+    await Categorie.deleteMany({});
+    server.close();
+  });
+
+  
+
+});
