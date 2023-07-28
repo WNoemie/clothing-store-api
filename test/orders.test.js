d2ca02e9d3e5f559246da879543c06f1c2968593
WNoemie <noemiewolf@gmail.com>
2023-07-28 16:04:44 +0200

    orders test get api/orders/:id

--- test/orders.test.js
+++ test/orders.test.js
@@ -2,27 +2,26 @@ const request = require('supertest');
 const { expect } = require('chai');
 const { Categorie } = require('../models/categorie');
 const { Shirt } = require('../models/shirt');
-const { User } = require('../models/user'); // Import the User model
-const { Order } = require('../models/order'); // Import the Order model
+const { User } = require('../models/user');
+const { Order } = require('../models/order');
 const existingToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGMwNjVjZTc5YjNlNDU5NmU5MTdlMWYiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2OTAzNjcwNzZ9.JBTKNtbrdztoyxmIEoJ_rtwEB5qrhdBKyIOAfnkZquc';
 
-let server; 
+let server;
+let user;
+let shirt1;
+let shirt2;
+let order;
 
 describe('Orders Routes', () => {
-  let user; 
-  let shirt1;
-  let shirt2;
-  let order;
-  
   before(async () => {
     server = require('../index');
     user = new User({
-        _id: '64c065ce79b3e4596e917e1f',
-        name: 'Maarten',
-        email: 'maarten@vives.be',
-        isAdmin: true,
-        password: '12345', 
-      });
+      _id: '64c065ce79b3e4596e917e1f',
+      name: 'Maarten',
+      email: 'maarten@vives.be',
+      isAdmin: true,
+      password: '12345',
+    });
     await user.save();
   });
 
@@ -61,7 +60,6 @@ describe('Orders Routes', () => {
     });
     await shirt2.save();
 
-    
     order = new Order({
       user: user._id,
       shirts: [
@@ -73,22 +71,45 @@ describe('Orders Routes', () => {
     await order.save();
   });
 
-  it('should return all orders from db', async () => {
-    const res = await request(server)
-      .get('/api/orders')
-      .set('x-auth-token', existingToken);
+  describe('GET /api/orders', () => {
+    it('should return all orders from db', async () => {
+      const res = await request(server)
+        .get('/api/orders')
+        .set('x-auth-token', existingToken);
 
-    expect(res.status).to.equal(200);
-    expect(res.body).to.be.an('array');
-    expect(res.body.length).to.equal(1);
-    expect(res.body[0].shirts).to.be.an('array');
-    expect(res.body[0].shirts.length).to.equal(2); 
-    
-    expect(res.body[0].shirts[0].shirt.beschrijving).to.equal('Gele shirt');
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('array');
+      expect(res.body.length).to.equal(1);
+      expect(res.body[0].shirts).to.be.an('array');
+      expect(res.body[0].shirts.length).to.equal(2);
+      expect(res.body[0].shirts[0].shirt.beschrijving).to.equal('Gele shirt');
+    });
+ });
 
+ describe('GET /api/orders/:id', () => {
+    it('should return a order with id for the authenticated user', async () => {
+      const res = await request(server)
+        .get(`/api/orders/${order._id}`)
+        .set('x-auth-token', existingToken);
+
+      expect(res.status).to.equal(200);
+      expect(res.body.shirts).to.be.an('array');
+      expect(res.body.shirts.length).to.equal(2);
+      
+    });
+
+    it('should return 404 if the order is not found', async () => {
+      const invalidId = '123456152345676543212345'; 
+
+      const res = await request(server)
+        .get(`/api/orders/${invalidId}`)
+        .set('x-auth-token', existingToken);
+
+      expect(res.status).to.equal(404);
+      expect(res.text).to.equal('Order niet gevonden.');
+      
+    });
   });
+
+ 
 });
-
-
-
-  
